36689531fdce5bb37723e57063b8a90197922fe4
Chelsea Owiti <chelseaowiti@gmail.com>
2021-08-26 14:09:28 +0300

    runtime exception

--- app/src/main/java/com/example/csp/ProfileActivity.java
+++ app/src/main/java/com/example/csp/ProfileActivity.java
@@ -15,13 +15,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
     private DrawerLayout drawer;
+    private Toolbar toolbars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_bar_main);
 
-        Toolbar toolbars = findViewById(R.id.toolbar);
+        toolbars = findViewById(R.id.toolbar);
         setSupportActionBar(toolbars);
 
         drawer = findViewById(R.id.drawer_layout);
