5f11c0873b8543af45d03b164f8945f6f05cd2c8
Chelsea Owiti <chelseaowiti@gmail.com>
2021-08-30 01:43:15 +0300

    firestore database tested

--- app/src/main/java/com/example/csp/RegisterUser.java
+++ app/src/main/java/com/example/csp/RegisterUser.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -15,21 +16,32 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegisterUser extends AppCompatActivity {
 
-     //TextView banner, registerUser;
+    public static final String TAG = "TAG";
+    //TextView banner, registerUser;
     EditText fullNames, phoneNumber, passwordReg, emailReg;
     ProgressBar progressBar;
     TextView mLoginBtn;
     FirebaseAuth mAuth;
     Button signUpBtn;
+
+    //fire store
+    FirebaseFirestore fStore;
+    String userID;
 
     public RegisterUser() {
     }
@@ -41,6 +53,8 @@ public class RegisterUser extends AppCompatActivity {
         setContentView(R.layout.activity_register_user);
 
         mAuth = FirebaseAuth.getInstance();
+        // fire base fire store instance
+        fStore = FirebaseFirestore.getInstance();
 //        banner = findViewById(R.id.banner);
 //        banner.setOnClickListener(this);
 //
@@ -103,6 +117,23 @@ public class RegisterUser extends AppCompatActivity {
                     public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(RegisterUser.this, "User Created", Toast.LENGTH_SHORT).show();
+                            //fire store implementation
+
+                            userID = mAuth.getCurrentUser().getUid();
+
+                            DocumentReference documentReference = fStore.collection("customers").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("fname", fullName);
+                            user.put("email", email);
+                            user.put("phone", phone);
+
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG, "onSuccess: User profile is created for "+userID);
+                                }
+                            });
+
                             startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
                         } else {
                             Toast.makeText(RegisterUser.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
