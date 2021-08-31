fbd1dbc1d90db7f8f1be394abf8070defba8b76f
Chelsea Owiti <chelseaowiti@gmail.com>
2021-08-31 15:20:07 +0300

    api key concealed

--- app/src/main/java/com/example/csp/MessageFragment.java
+++ app/src/main/java/com/example/csp/MessageFragment.java
@@ -1,4 +1,8 @@
 package com.example.csp;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -6,20 +10,30 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import org.jetbrains.annotations.NotNull;
 
 
 //actually for maps not messaging
-public class MessageFragment extends Fragment{
+public class MessageFragment extends Fragment implements OnMapReadyCallback {
+
+    Location currentLocation;
+    FusedLocationProviderClient client;
+    private static final int REQUEST_CODE = 101;
+
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
@@ -28,8 +42,13 @@ public class MessageFragment extends Fragment{
         //initialize view
         View view = inflater.inflate(R.layout.fragment_message, container, false);
 
+
+        //initialize fused location
+//        client = LocationServices.getFusedLocationProviderClient(this);
+
+
         //initialize map fragment
-        SupportMapFragment supportMapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.maps);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.maps);
 
         //async map
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
@@ -60,11 +79,74 @@ public class MessageFragment extends Fragment{
 
             }
         });
+//        if (ActivityCompat.checkSelfPermission(ProfileActivity.this,
+//                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//
+//            //When permission is granted call method
+//
+//            getCurrentLocation();
+//
+//        }
 
         //return view
         return view;
 
 
+    }
+
+    private void getDeviceLocation() {
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task location = client.getLastLocation();
+
 
     }
+
+//    private void getCurrentLocation() {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        Task<Location> task = client.getLastLocation();
+//        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+//            @Override
+//            public void onSuccess(Location location) {
+//                //success
+//                if (location != null){
+//                    supportMapFragment.getMapAsync(new OnMapReadyCallback()){
+//
+//                    }
+//                }
+//            }
+//        });
+//
+//    }
+
+    @Override
+    public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+
+    }
+
+//    Location currentLocation;
+//    FusedLocationProviderClient fusedLocationProviderClient;
+//    private static final int REQUEST_CODE = 101;
+
+
 }
